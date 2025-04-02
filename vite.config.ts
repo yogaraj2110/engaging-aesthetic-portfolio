62aa6bcbfe79ab8e2d24591d606e88c887e4f646
yogaraj2110 <127007342+yogaraj2110@users.noreply.github.com>
2025-04-02 16:45:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
